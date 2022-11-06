374ab3f99a911f3b5499add8f5d1076838f62107
Husen Efendi <husenef@gmail.com>
2022-11-06 23:23:12 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      raleway: ['Raleway', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
